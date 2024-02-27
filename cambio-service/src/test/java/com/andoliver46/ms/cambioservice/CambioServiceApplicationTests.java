7c2a9ddc573ac50eb802df6cb5457bdc4c6d22c5
AndOliver46 <anderson_souza75@live.com>
2024-02-27 10:58:41 -0300

    feat: cambio service created and configured (port, name, configserver address)

---
+++ cambio-service/src/test/java/com/andoliver46/ms/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
