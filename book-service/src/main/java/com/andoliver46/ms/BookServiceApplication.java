639309933332fa03aea4cec770feefb9c659784a
AndOliver46 <anderson_souza75@live.com>
2024-03-06 11:32:45 -0300

    feat: book-service created and configured port, name, config server address

---
+++ book-service/src/main/java/com/andoliver46/ms/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
