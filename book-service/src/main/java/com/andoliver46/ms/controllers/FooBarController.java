50a498af85de88888de1bec505082cc80bd31e8b
AndOliver46 <anderson_souza75@live.com>
2024-03-11 19:09:30 -0300

    feat: adding bulkHead annotation to foobar metho, to limit concurrent calls on method to 10

--- book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
+++ book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
@@ -19,7 +20,8 @@ public class FooBarController {
 	@GetMapping("/foo-bar")
 	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
-	@RateLimiter(name = "default", fallbackMethod = "fallbackMethod")
+	//@RateLimiter(name = "default", fallbackMethod = "fallbackMethod")
+	@Bulkhead(name = "default")
 	public String fooBar() {
 		LOGGER.info("Request to foo-bar is received!");
 		//ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
