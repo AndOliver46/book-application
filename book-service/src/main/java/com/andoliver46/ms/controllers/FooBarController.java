048abb06abfc6fd5df10e0d867932f5676dc5176
AndOliver46 <anderson_souza75@live.com>
2024-03-11 19:06:45 -0300

    feat: adding rateLimiter to foobar method, with config: in 10s max 2 calls per instance os microservice

--- book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
+++ book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 @RequestMapping("book-service")
@@ -19,11 +18,13 @@ public class FooBarController {
 	
 	@GetMapping("/foo-bar")
 	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	@RateLimiter(name = "default", fallbackMethod = "fallbackMethod")
 	public String fooBar() {
 		LOGGER.info("Request to foo-bar is received!");
-		ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-		return response.getBody();
+		//ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		//return response.getBody();
+		return "foo-bar";
 	}
 	
 	public String fallbackMethod(Exception ex) {
