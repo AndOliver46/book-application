dff11f4bd4bb0bc8a1fb501e879822a1f3ff4909
AndOliver46 <anderson_souza75@live.com>
2024-03-11 10:44:31 -0300

    feat: adding api-gateway service with configs: server port, app name, eureka address, config server address

---
+++ api-gateway/src/main/java/com/andoliver46/ms/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/andoliver46/ms/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
