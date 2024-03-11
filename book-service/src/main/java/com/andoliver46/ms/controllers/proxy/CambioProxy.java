92c7853989314662e851293f23a283c559bb6fc2
AndOliver46 <anderson_souza75@live.com>
2024-03-11 10:10:05 -0300

    feat: removing hardcoded Cambio-service address from feign CambioProxy for automatic load balancing by eureka

--- book-service/src/main/java/com/andoliver46/ms/controllers/proxy/CambioProxy.java
+++ book-service/src/main/java/com/andoliver46/ms/controllers/proxy/CambioProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.andoliver46.ms.dtos.CambioDTO;
 
-@FeignClient(name = "cambio-service", url = "http://localhost:8000")
+@FeignClient(name = "cambio-service")
 public interface CambioProxy {
 	
 	@GetMapping("/cambio-service/{amount}/{from}/{to}")
