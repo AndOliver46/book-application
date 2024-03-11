1eff00f94372f1227bdafd9efc2e98f872fd09f1
AndOliver46 <anderson_souza75@live.com>
2024-03-11 18:34:54 -0300

    feat: retry annotation in foobar controller, using custom profile from properties with 5 attemps of 0.51s

--- book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
+++ book-service/src/main/java/com/andoliver46/ms/controllers/FooBarController.java
@@ -1,17 +1,27 @@
 package com.andoliver46.ms.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/foo-bar")
+	@Retry(name = "foo-bar")
 	public String fooBar() {
-		return "Foo-Bar!!";
+		LOGGER.info("Request to foo-bar is received!");
+		ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		return response.getBody();
 	}
 	
 }
